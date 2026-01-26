38dc0d9702cd86d0cde278e1dcda1cb421008520
InfinityLoop1308 <96324692+InfinityLoop1308@users.noreply.github.com>
2026-01-26 18:18:58 +0800

    v1.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "project.pipepipe.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 1000
-        versionName = "1.0.0"
+        versionCode = 1001
+        versionName = "1.0.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
